14e25c176c0626a309b9a16561c1f46311573b8e
Axel Aquino <96102202+aquinoax3@users.noreply.github.com>
2025-04-06 00:33:37 -0400

    Update split_strings.cpp

--- split_strings.cpp
+++ split_strings.cpp
@@ -4,3 +4,31 @@
 
 // * 'abc' =>  ['ab', 'c_']
 // * 'abcdef' => ['ab', 'cd', 'ef']
+
+
+#include <string>
+#include <vector>
+
+std::vector<std::string> solution(const std::string &s) {
+  std::vector<std::string> result;
+  int left = 0;
+  
+  for (int right = 0; right < s.size(); right++) {
+    if (right % 2 != 0) {
+      std::string pair = s.substr(left, right - left + 1);
+      result.push_back(pair);
+      left = right + 1;
+    } else if (right == s.size() - 1) {
+      std::string pair = "";
+      pair.push_back(s[right]);
+      pair.push_back('_');
+      result.push_back(pair);
+    }
+  }
+  
+  return result;
+}
+
+//    "abcde"
+//       l
+//        r
