471e8caa195be20f4e67313a680d14cbe288ed4a
Axel Aquino <96102202+aquinoax3@users.noreply.github.com>
2025-04-06 23:29:28 -0400

    Create the_vowel_code.cpp

---
+++ the_vowel_code.cpp
@@ -0,0 +1,46 @@
+// Step 1: Create a function called encode() to replace all the lowercase vowels in a given string with numbers according to the following pattern:
+
+// a -> 1
+// e -> 2
+// i -> 3
+// o -> 4
+// u -> 5
+// For example, encode("hello") would return "h2ll4". There is no need to worry about uppercase vowels in this kata.
+
+// Step 2: Now create a function called decode() to turn the numbers back into vowels according to the same pattern shown above.
+
+// For example, decode("h3 th2r2") would return "hi there".
+
+// For the sake of simplicity, you can assume that any numbers passed into the function will correspond to vowels.
+
+
+
+
+#include <string>
+
+std::string encode(const std::string &str) {
+  std::string word = str;
+  std::unordered_map<char, int> vowels = {
+    {'a', 1},
+    {'e', 2},
+    {'i', 3},
+    {'o', 4},
+    {'u', 5}
+  };
+  
+  std::string result = "";
+  
+  for (char el : word) {
+    if (vowels.find(el) != vowels.end()) {
+      result += std::to_string(vowels[el]);
+    } else {
+      result += el;
+    }
+  }
+  
+  return result;
+}
+
+// std::string decode(const std::string &str) {
+//   return "";
+// }
