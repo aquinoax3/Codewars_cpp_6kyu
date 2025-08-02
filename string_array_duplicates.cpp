aae9926ebbbc18d34b22c103eca160cd176a3770
Axel Aquino <96102202+aquinoax3@users.noreply.github.com>
2025-08-01 23:57:48 -0400

    Create string_array_duplicates.cpp

---
+++ string_array_duplicates.cpp
@@ -0,0 +1,17 @@
+// In this Kata, you will be given an array of strings and your task is to remove all consecutive duplicate letters from each string in the array.
+
+// For example:
+
+// dup(["abracadabra","allottee","assessee"]) = ["abracadabra","alote","asese"].
+
+// dup(["kelless","keenness"]) = ["keles","kenes"].
+
+// Strings will be lowercase only, no spaces. See test cases for more examples.
+
+// Good luck!
+
+#incluce
+
+std::vector<std::string> dup(std::vector<std::string> arr){    
+    //..
+}
