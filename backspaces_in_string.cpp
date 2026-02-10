32e6bb7b2a74fca489d3e315e167e723e751350c
Axel Aquino <96102202+aquinoax3@users.noreply.github.com>
2026-02-09 20:19:47 -0500

    Update backspaces_in_string.cpp

--- backspaces_in_string.cpp
+++ backspaces_in_string.cpp
@@ -10,7 +10,23 @@
 
 
 
-
 std::string cleanString(const std::string &s) {
-    // your code here
+  int backspaces = 0;
+  int letters = 0;
+  
+  for (char el : s) {
+    if (isalpha(el)) {
+      letters++;
+    } else {
+      backspaces++;
+    }
+  }
+  
+  if (backspaces > letters) {
+    return "";
+  }
+  
+  
+  
+  return "";
 }
